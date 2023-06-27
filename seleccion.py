b23aedc7b3147cef1dd9fedafdcd000e04b1c8c1
FerretyPrune620 <donideltoro@hotmail.com>
2023-06-26 18:08:52 -0600

    Se modifico el archivo .gitignore y también se carga el archivo seleccion.py

---
+++ seleccion.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import re
+
+with open(ruta, 'r') as f:
+    text = f.read()
+
+    lines = text.split('\n')
+
+    contador=0
+    for line in lines:
+        contador+=1
+        match = re.search('^\d{1,2}/\d{1,2}/\d{2,4}', line)
+        if match:
+            break
+        
+with pd.read_csv(archivo, chunksize=10000, skiprows=contador-2) as reader:
+    for j, chunk in enumerate(reader):
+        totalshape=totalshape+len(chunk)
+        if j==0:
+            encabezados=list(chunk.columns)
+
+        chunk['Times'] = pd.to_datetime(chunk['Time']).dt.time
+        chunk['Times_sec'] = chunk['Times'].apply(time_to_sec)
+        listilla.append(chunk)
